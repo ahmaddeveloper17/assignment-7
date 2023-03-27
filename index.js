51f0c0c8c04bf4752d77946ac2c820d99edd295f
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-27 19:36:36 +0500

    assignment-7

---
+++ index.js
@@ -0,0 +1,23 @@
+{var a=5,b=3;
+    console.log("a is a maximum number.");
+}
+{
+    var x=15,y=35,z=29;
+    console.log("y is a maximum number.");
+}
+{
+    var usernumber =("-45 ");
+    if(usernumber > 0)
+    {
+        console.log("The number is positive.");
+    }
+   else if(usernumber < 0)
+    {
+        console.log("The number is nagetive.");
+    }
+   else if(usernumber == 0)
+    {
+        console.log("The number is zero.");
+    }
+}
+
