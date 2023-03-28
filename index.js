75cfa37994ad8d81e6e2af035981d3d8e08ad2ad
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-28 19:34:06 +0500

    assignment-7

--- index.js
+++ index.js
@@ -20,4 +20,85 @@
         console.log("The number is zero.");
     }
 }
-
+{
+    var usernumber = ("110");
+    if (usernumber % 5 == 0 && usernumber % 11 == 0 )
+    {
+        console.log("This number is divisible by 5 or 11");
+    }
+}
+{
+    var usernumber = ("70");
+    if (usernumber % 2 == 0)
+    {
+        console.log("%d is a even number" ,usernumber);
+    }
+    else 
+    {
+        console.log("%d is a odd number" ,usernumber);
+    }
+}
+{
+    var year = ("2024");
+    if (year % 4 == 0)
+    {
+        console.log("%d is a leap year" ,year);
+    }
+    else
+    {
+        console.log("%d is not a leap year" ,year);
+    }
+}
+{
+    var ch = ("G");
+if(ch > "a" && ch < "z" )
+    console.log("This character is a alphabet");
+else if (ch > "A" && ch < "Z")
+    console.log("This character is a alphabet");
+else
+    console.log("This character is not a alphabet");   
+}
+{
+    var vowel="o"
+if(vowel=="a")
+     console.log("Your entered word is vowel");
+else if(vowel=="e")
+     console.log("Your entered word is vowel");
+else if(vowel=="i")
+     console.log("Your entered word is vowel");
+else if(vowel=="o")
+     console.log("Your entered word is vowel");
+else if(vowel=="u")
+     console.log("Your entered word is vowel");
+else if(vowel=="A")
+     console.log("Your entered word is vowel");
+else if(vowel=="E")
+     console.log("Your entered word is vowel");
+else if(vowel=="I")
+     console.log("Your entered word is vowel");
+else if(vowel=="O")
+     console.log("Your entered word is vowel");
+else if(vowel=="U")
+     console.log("Your entered word is vowel");
+else
+    console.log("Your entered word is not a vowel");
+}
+{
+    var ch = ("@")
+if (ch > "a" && ch < "z")
+    console.log("Your entered word is alphabet");
+else if (ch > "A" && ch < "Z")
+    console.log("Your entered word is alphabet");
+else if (ch > "1" && ch < "9")
+    console.log("Your entered word is digit");
+else
+    console.log("Your entered word is special character");
+}
+{
+    var ch = ("R")
+if (ch > "A" && ch < "Z")
+    console.log("Your entered word is uppercase alphabet");
+else if (ch > "a" && ch < "z")
+    console.log("Your entered word is lowercase alphabet");
+}
+ 
