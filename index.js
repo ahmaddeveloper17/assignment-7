67b1302d33e715dbb881d8f8d704eedb1d9bf242
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2023-03-29 17:25:17 +0500

    assignment-7

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// [program no: 1]
 {var a=5,b=3;
     console.log("a is a maximum number.");
 }
+// [program no: 2]
 {
     var x=15,y=35,z=29;
     console.log("y is a maximum number.");
 }
+// [program no: 3]
 {
     var usernumber =("-45 ");
     if(usernumber > 0)
@@ -20,6 +23,7 @@
         console.log("The number is zero.");
     }
 }
+// [program no: 4]
 {
     var usernumber = ("110");
     if (usernumber % 5 == 0 && usernumber % 11 == 0 )
@@ -27,6 +31,7 @@
         console.log("This number is divisible by 5 or 11");
     }
 }
+// [program no: 5]
 {
     var usernumber = ("70");
     if (usernumber % 2 == 0)
@@ -38,6 +43,7 @@
         console.log("%d is a odd number" ,usernumber);
     }
 }
+// [program no: 6]
 {
     var year = ("2024");
     if (year % 4 == 0)
@@ -49,6 +55,7 @@
         console.log("%d is not a leap year" ,year);
     }
 }
+// [program no: 7]
 {
     var ch = ("G");
 if(ch > "a" && ch < "z" )
@@ -58,6 +65,7 @@ else if (ch > "A" && ch < "Z")
 else
     console.log("This character is not a alphabet");   
 }
+// [program no: 8]
 {
     var vowel="o"
 if(vowel=="a")
@@ -83,6 +91,7 @@ else if(vowel=="U")
 else
     console.log("Your entered word is not a vowel");
 }
+// [program no: 9]
 {
     var ch = ("@")
 if (ch > "a" && ch < "z")
@@ -94,11 +103,40 @@ else if (ch > "1" && ch < "9")
 else
     console.log("Your entered word is special character");
 }
+// [program no: 10]
 {
     var ch = ("R")
 if (ch > "A" && ch < "Z")
     console.log("Your entered word is uppercase alphabet");
 else if (ch > "a" && ch < "z")
     console.log("Your entered word is lowercase alphabet");
+}
+// {
+//     var weak = ("1")
+//     if (weak )
+// }
+// [program no: 12]
+{
+    var month = (2)
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 ||month == 12)
+        console.log("month no:%d has 31 days",month);
+    else if (month == 2)
+        console.log("%dnd month has 28 days",month);    
+    else
+        console.log("month no:%d has 30 days",month);    
+}
+// [program no: 13]
+{
+    var a=105,b=10,c=45;
+    z = a*(c-b);
+    console.log("The answer of z is",z);
+}
+// [program no: 14]
+{
+    var a=45,b=47,c=45;
+    if ( a==b && b==c && c==a)
+      console.log("Triangle is valid ");
+    else
+      console.log("Triangle is not valid ");  
 }
  
